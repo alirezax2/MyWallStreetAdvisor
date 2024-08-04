5a972b7c81b2ccac7eedd748e3cf01fabba850a7
Alireza <alireza.naziri@gmail.com>
2024-08-04 08:51:56 +0000

    run npm run deploy

--- mywallstreetadvisor/next.config.js
+++ mywallstreetadvisor/next.config.js
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  output: 'export',
   basePath: isProd ? '/mywallstreetadvisor' : '',
   assetPrefix: isProd ? '/mywallstreetadvisor/' : '',
 };
